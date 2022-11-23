720c92a1aa2586bb01909749ccfcfce0a3858df2
TamzinC <115940593+TamzinC@users.noreply.github.com>
2022-11-23 22:10:05 +0000

    Completed code. Added if statements to account for the different user options, added the expression for the getRandom function and set up a for loop to put together the password options as chosen by the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,18 +88,7 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Combining all arrays into one big array:
-var charactersArray = {specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters}
-
-// // testing to see if the above worked!
-// console.log(charactersArray);
-// console.log(charactersArray.specialCharacters);
-
-
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-  // Steps needed from user:
+ // Steps needed from user:
   // Choose their password length, up to 64 characters
   // Ask if they'd like lowercase to be included
   // Ask if they'd like uppercase included
@@ -107,22 +96,14 @@ function getPasswordOptions() {
   // Ask if they'd like special characters included
   // Make sure at least one character type is selected!
 
-
-  // var passLength = 64;
-  // var randomPass = charactersArray[Math.floor(Math.random() * passLength.length)];
-  // var userInput = prompt('Choose your password length (10 - 64)');
-  
-  
-  // for (var count = 1; count <= passLength; count++) {
-  //   console.log(count);
-  // }
-
-
-}
+// // Function to prompt user for password options - decided to not use to function and instead keep majority of the code under one function to avoid complexity and repitition
+// function getPasswordOptions() {
+ 
+// }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[Math.floor(Math.random()*arr.length)];
 }
 
 // Function to generate password with user input
@@ -131,16 +112,50 @@ function generatePassword() {
 
   console.log(passwordLength);
 
-  if (isNan(passwordLength) || passwordLength < 10) {
+  if (isNaN(passwordLength) || passwordLength < 10) {
     alert("Password too short!");
-  } else if (passwordLength > 64) {
+    return;
+  } 
+  if (passwordLength > 64) {
     alert("Password too long!");
     return;
   }
   
   console.log("Good Length");
   
-  
+  var includeLowercase = confirm("Would you like to include lowercase letters? (Click 'OK' for Yes or 'Cancel' for No)");
+  var includeUppercase = confirm("Would you like to include uppercase letter? (Click 'OK' for Yes or 'Cancel' for No)");
+  var includeNumbers = confirm("Would you like to include any numbers? (Click 'OK' for Yes or 'Cancel' for No)");
+  var includeSpecial = confirm("Would you like to include any special characters? (Click 'OK' for Yes or 'Cancel' for No)");
+
+  var characterOptions = [];
+  if (includeLowercase) {
+    characterOptions = characterOptions.concat(lowerCasedCharacters);
+  }
+  if (includeUppercase) {
+    characterOptions = characterOptions.concat(upperCasedCharacters);
+  }
+  if (includeNumbers) {
+    characterOptions = characterOptions.concat(numericCharacters);
+  }
+  if(includeSpecial) {
+    characterOptions - characterOptions.concat(specialCharacters);
+  }
+
+  if (characterOptions.length === 0) {
+    alert("Failed to provide appropiate password options!");
+    return "";
+  }
+
+  console.log(characterOptions);
+
+  var password = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    password += getRandom(characterOptions);
+  }
+
+  return password;
 }
 
 // Get references to the #generate element
